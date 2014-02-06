dc9e3efb66b352fc8ff0513477175e5b60714a8b
jhmatthes <jaclyn.hatala.matthes@gmail.com>
2014-02-06 17:39:49 -0500

    minor bug fixes

--- SAS_spinup/restart_spinup_histo.sh
+++ SAS_spinup/restart_spinup_histo.sh
@@ -12,37 +12,47 @@ then
     echo "ERROR: you must run make_spinup_subdirs.sh before this!"
 fi
 
-#while true
-#do
+while true
+do
+    sleep 600 #only run every 5 minutes
+
     for SITE in ${sites[@]}
     do
 	for n in {1..15}
 	do
+	    #check if site rep is running
 	    rep=$(printf "%02d" ${n})
 	    path=${outdir}${SITE}/spin${rep}/
-	    runstat=$(qstat -u $USER | grep ${SITE}spin${rep} | wc -l)
+	    runstat=$(qstat -u ${USER} | grep ${SITE}spin${rep} | wc -l)
 
+	    #get dates of last histo file
 	    lastday=`ls -l -rt ${path}/histo| tail -1 | rev | cut -c15-16 | rev`
 	    lastmonth=`ls -l -rt ${path}/histo| tail -1 | rev | cut -c18-19 | rev`
 	    lastyear=`ls -l -rt ${path}/histo| tail -1 | rev | cut -c21-24 | rev`
 	    echo $lastyear
 	    echo $runstat
 
-	    #if run stopped and histo file last year is less than final year
+	    #if run has stopped and last year in histo directory is less than the final run year
 	    if [[ "${runstat}" -eq 0 && "${lastyear}" -lt "${finalyear}" ]]
 	    then
 		pushd ${path} 
 		echo "got here!"
 
 		#edit ED2IN dates for restart
-		
+		sed -i "s/IYEARA   = .*/IYEARA   = ${lastyear}   ! Year/" ED2IN 
+		sed -i "s/IDATEA   = .*/IDATEA   = ${lastday}     ! Day/" ED2IN 
+		sed -i "s/IMONTHA  = .*/IMONTHA  = ${lastmonth}     ! Month/" ED2IN 
+		sed -i "s/IYEARH   = .*/IYEARH   = ${lastyear}   ! Year/" ED2IN 
+		sed -i "s/IDATEH   = .*/IDATEH   = ${lastday}     ! Day/" ED2IN 
+		sed -i "s/IMONTHH  = .*/IMONTHH  = ${lastmonth}     ! Month/" ED2IN 
 
-		#edit ED2IN params for restart
-
+		#edit ED2IN params for history restart
+		sed -i 's/IED_INIT_MODE   = .*/IED_INIT_MODE   = 5/' ED2IN
+		sed -i "s/RUNTYPE  = .*/RUNTYPE  = 'HISTORY'/" ED2IN
 
 #	        qsub paleon_ed2_geo.sh
 		popd
 	    fi
 	done
     done
-#done
+done
