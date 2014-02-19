cadbe4a3e2417351b934177880d22d9fd832a718
jhmatthes <jaclyn.hatala.matthes@gmail.com>
2014-02-19 16:44:17 -0500

    ED code additions for soil SAS

--- SAS_spinup/compile_SAS_runs.R
+++ SAS_spinup/compile_SAS_runs.R
@@ -16,6 +16,22 @@ pft   <- c(5,6,8,9,10,11) #set of PFTs used in analysis
 dpm <- c(31,28,31,30,31,30,31,31,30,31,30,31)
 sufx  <- "g01.h5"
 
+#constants from ED2 for SAS solution for soil pools
+fsc_loss <- 1.0/11.0
+ssc_loss <- 1.0/100.2
+ssl_loss <- 1.0/5.4
+resp_temperature_increase <- 0.0757
+resp_opt_water            <- 0.8938
+resp_water_below_opt      <- 5.0786
+Lc                        <- 0.049787
+c2n_slow                  <- 10.0
+c2n_structural            <- 150.0
+r_stsc                    <- 0.3
+soil_tempk <- c(278.5,279.3, 280.0, 277.3, 276.8, 277.2) #mean temp per site
+temperature_limitation = resp_temperature_increase * exp(308.56 * (1./56.02 - 1./(soil_tempk-227.15)))
+water_limitation <- exp((0.85 - resp_opt_water) * resp_water_below_opt)
+A_decomp <- temperature_limitation * water_limitation
+
 #First loop over analy files (faster than histo) to aggregate initial 
 #.css and .pss files for each site
 for(s in sites){
@@ -101,8 +117,8 @@ for(s in sites){
 #for steady-state solution
 
 #storage
-fsc_in_y <- ssc_in_y <- ssl_in_y <- vector()
-fsc_in_m <- ssc_in_m <- ssl_in_m <- vector()
+fsc_in_y <- ssc_in_y <- ssl_in_y <- fsn_in_y <- pln_up_y <- vector()
+fsc_in_m <- ssc_in_m <- ssl_in_m <- fsn_in_m <- pln_up_m <-  vector()
 
 for(s in sites){
   dat.dir    <- paste(base,s,"/spin01/histo/",sep="")
@@ -148,13 +164,32 @@ for(s in sites){
       ssc_in_m[m-month.begin+1] <- get.var.ncdf(now,"SSC_IN")*dpm[m]
       ssl_in_m[m-month.begin+1] <- get.var.ncdf(now,"SSL_IN")*dpm[m]
       fsn_in_m[m-month.begin+1] <- get.var.ncdf(now,"FSN_IN")*dpm[m]
+      pln_up_m[m-month.begin+1] <- get.var.ncdf(now,"TOTAL_PLANT_NITROGEN_UPTAKE")*dpm[m]
+      
     }
     fsc_in_y[y-yeara+1] <- sum(fsc_in_m,na.rm=TRUE)
     ssc_in_y[y-yeara+1] <- sum(ssc_in_m,na.rm=TRUE)
     ssl_in_y[y-yeara+1] <- sum(ssl_in_m,na.rm=TRUE)
     fsn_in_y[y-yeara+1] <- sum(fsn_in_m,na.rm=TRUE)
+    pln_up_y[y-yeara+1] <- sum(pln_up_m,na.rm=TRUE)
     
     close.ncdf(now)
   }
   
+  #Calculate steady-state soil pools
+  fsc_ss <- median(fsc_in_y)/(fsc_loss * A_decomp)
+  ssc_ss <- median(ssc_in_y)/(ssc_loss * A_decomp)
+  ssl_ss <- median(ssl_in_y)/(ssl_loss * A_decomp * Lc)
+  fsn_ss <- median(fsn_in_y)/(fsc_loss * A_decomp)
+  
+  #fast_N_loss + slow_C_loss
+  msn_med  <- fsc_loss*A_decomp*median(fsn_in_y)+ (ssc_loss * A_decomp)/c2n_slow 
+  
+  #ED2: csite%mineralized_N_loss  = csite%total_plant_nitrogen_uptake(ipa)             
+  # + csite%today_Af_decomp(ipa) * Lc * K1 * csite%structural_soil_C(ipa)                     
+  # * ( (1.0 - r_stsc) / c2n_slow - 1.0 / c2n_structural)
+  msn_loss <- median(pln_up_y) + A_decomp*Lc*ssl_loss*median(ssl_in_y)*
+    ((1.0-r_stsc)/c2n_slow - 1.0/c2n_structural)
+  
+  msn_ss   <- msn_med/msn_loss
 }
